fdb0c27182b051c7d6c32ecc7a2519bde9998e83
Alexander Perez <116762210+Pappiburr@users.noreply.github.com>
2025-06-12 12:53:05 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
 import ReactDom from 'react-dom/client'
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import { BrowserRouter} from 'react-router-dom'
 import './app.css'
 import App from './App.tsx'

a6b046c7ce963a7d4d56684454219a93c1468704
Alexander Perez <116762210+Pappiburr@users.noreply.github.com>
2025-06-19 13:17:58 -0500

    Update nav.tsx

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,17 +1,15 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
 import ContactButton from '../contact-button/contact-button'
 
 const Nav: React.FC = () => {
 
-  const navigate = useNavigate();
   return(
     <div className='Flex flex-row justify-between items-center'>
       
 
       <div className=' grid grid-cols-5 gap-1  outline-1 box-shadow-lg'>
         <div className='text-4xl font-bold pt-9 ps-4  pe-5 text-shadow-black'>Skydrop Studios</div>
-        <div className='ps-30 text-shadow-black ' onClick={() => navigate('/home')}>Github</div>
+        <div className='ps-30 text-shadow-black'><a href="github.com"></a></div>
         <div className='ps-30 text-shadow-black '><ContactButton/></div>
         <div className='ps-30 text-shadow-black'>Tip Jar</div>
       </div>
